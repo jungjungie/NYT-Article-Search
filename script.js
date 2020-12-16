32bfb152bc243cd84b1ddc79621810414070cc0a
Esther Min <estherjmin@gmail.com>
2020-12-16 14:41:36 -0500

    set the top articles section to be hidden when there are no search results displayed

--- script.js
+++ script.js
@@ -20,8 +20,10 @@ $("#searchBtn").on("click", function (event) {
         console.log(articleResults.response.docs);
         let articleData = articleResults.response.docs;
 
+        $("#results-section").attr("class", "card shadow-sm show");
+
         // Loop through the results for the number of times set in the search parameters
-        for (let i=0; i < numberOfRecords; i++) {
+        for (let i = 0; i < numberOfRecords; i++) {
 
             // Create a new div and new elements for each search result
             let resultCard = $("<div>").attr("class", "card p-4 result-cards");
@@ -30,7 +32,7 @@ $("#searchBtn").on("click", function (event) {
             let author = $("<p>").text(articleData[i].byline.original).attr("class", "author");
             let date = $("<p>").text("Published on " + articleData[i].pub_date).attr("class", "published-date");
             let summary = $("<p>").text(articleData[i].snippet);
-            let url = $("<a>").text("Read the full article here.").attr("href", articleData[i].web_url);
+            let url = $("<a>").text("Read the full article here.").attr("href", articleData[i].web_url).attr("target", "_blank");
 
             // Append new elements to new resultCard div
             resultCard.append(headline, author, date, summary, url);
@@ -40,3 +42,7 @@ $("#searchBtn").on("click", function (event) {
         }
     })
 })
+
+// Use moment.js to fix date display in results
+// Add Start Year and End Year into ajax call for search parameters
+// Add functionality to clear results btn
